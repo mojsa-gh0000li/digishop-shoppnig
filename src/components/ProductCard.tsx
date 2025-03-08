952c4976fb373fc5bd3ef54172182edb2b7cda62
a.zademehdi <alireza.zm734@gmail.com>
2025-03-08 16:38:29 +0330

    change img tag to image

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Product } from "@/lib/api";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className="max-w-52 border flex flex-col items-center rounded-lg p-3">
-      <img
+      <Image
         src={`https://apidigishop.narinsoft.ir/${product.image}`}
         alt={product.title}
         className="w-40 h-fit object-cover rounded-lg overflow-hidden"
       />
+      
       <h2 className="text-lg font-bold">{product.title}</h2>
       <p className="text-gray-700">
         {Number(product.price).toLocaleString()} تومان
